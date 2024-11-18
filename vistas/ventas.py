29423e9995f93103edf4f6180364e37d535f9044
jocelyngarcia10 <joselinnoemigarcia@gmail.com>
2024-11-18 14:04:15 -0600

    ventas

--- vistas/ventas.py
+++ vistas/ventas.py
@@ -1,15 +1,40 @@
-import streamlit as st
+import streamlit as  st
 import pandas as pd
 import plotly.express as px
-import datetime
 
-st.subheader("Filtrar Datos y Captura de Datos")
-st.write("El procesamiento de datos a traves de ciencia de Datos usando Streamlit de Python")
 
-dfDatos = pd.read_csv('http://raw.githubusercontent.com/gcastano/datasets/main/gapminder_data.csv')
 
-#Mostrar tabla de los registros 
-#dfDatos=dfDatos[(dfDatos[""])]
 
-st.metric("***Registros Totales***", len (dfDatos))
+
+#pip install plotly installar para la conexión de los datos
+
+st.header("Filtrado y Captura de Datos")
+st.write("EL procesamiento de datos a través de ciencia de datos usando Streamlit de Python.")
+
+#columnas///
+c1, c2, c3 = st.columns(3)
+
+with c1:
+    par_nombrePais = st.text_input("Pais:", placeholder="Escribe el nombre de un pais")
+
+with c2:
+    par_fertilidad = st.number_input("Minimo de numero de Hijos:", min_value=0, max_value=100, step=1)
+
+with c3: 
+    par_rangoExpectativaVida=st.slider("Rango de expectativa de vida:", min_value=10, max_value=100, value=(10,10))
+
+
+dfDatos = pd.read_csv('https://raw.githubusercontent.com/gcastano/datasets/main/gapminder_data.csv')
+
+#Seleccionar datos de la tabla
+
+if par_nombrePais !="":
+    dfDatos=dfDatos[dfDatos["country"].str.upper().str.contains(par_nombrePais.upper())]
+
+if par_fertilidad>0:
+    dfDatos=dfDatos[dfDatos["fertility"]>= par_fertilidad]
+
+dfDatos=dfDatos[(dfDatos["lifeExpectancy"]>=par_rangoExpectativaVida[0]) & (dfDatos["lifeExpectancy"]<=par_rangoExpectativaVida[1])]
+
+st.metric("Registros Totales", len (dfDatos))
 st.dataframe(dfDatos, use_container_width=True)
